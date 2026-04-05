1a3cf784862ec3dabc2aec81f23a6186a1eb2b82
kodnova <Dev@KodNova.dev>
2026-04-04 22:10:54 -0500

    add: debug lines

--- flake.nix
+++ flake.nix
@@ -273,6 +273,16 @@
           relativenumber = true;
           laststatus = 3;
         };
+        diagnostic.settings = {
+          virtual_lines = {
+            current_line = true;
+          };
+          virtual_text = false;
+          signs = true;
+          underline = true;
+          update_in_insert = false;
+          severity_sort = true;
+        };
         keymaps = [
           {
             key = "<C-s>";
